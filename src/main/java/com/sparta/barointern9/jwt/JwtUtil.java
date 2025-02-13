a9a37b2029bb3a31784e0f920ee6567e1fbba318
inzulmi <inzulmi132@gmail.com>
2025-02-13 21:52:10 +0900

    feat: [#7] Access / Refresh Token 검증 기능 추가

--- src/main/java/com/sparta/barointern9/jwt/JwtUtil.java
+++ src/main/java/com/sparta/barointern9/jwt/JwtUtil.java
@@ -1,7 +1,8 @@
 package com.sparta.barointern9.jwt;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.sparta.barointern9.exception.CustomApiException;
+import com.sparta.barointern9.exception.ErrorCode;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -37,5 +38,23 @@ public class JwtUtil {
                 .setExpiration(expiration)
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch(SecurityException | MalformedJwtException e) {
+            throw new CustomApiException(ErrorCode.INVALID_JWT_SIGN);
+        } catch(ExpiredJwtException e) {
+            throw new CustomApiException(ErrorCode.EXPIRED_JWT_TOKEN);
+        } catch(UnsupportedJwtException e) {
+            throw new CustomApiException(ErrorCode.UNSUPPORTED_JWT_TOKEN);
+        } catch(IllegalArgumentException e) {
+            throw new CustomApiException(ErrorCode.WRONG_JWT_TOKEN);
+        }
     }
 }
