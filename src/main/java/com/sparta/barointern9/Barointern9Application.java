958f6ea9eb2268c3f31e930d30863d72f4d2ed07
inzulmi <inzulmi132@gmail.com>
2025-02-13 15:54:45 +0900

    feat: init

---
+++ src/main/java/com/sparta/barointern9/Barointern9Application.java
@@ -0,0 +1,13 @@
+package com.sparta.barointern9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Barointern9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Barointern9Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/barointern9/Barointern9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.barointern9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Barointern9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
