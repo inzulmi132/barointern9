bfd6c7db2f56a75c5d6651ef6d7fce45433cc1c2
inzulmi <inzulmi132@gmail.com>
2025-02-13 22:20:28 +0900

    feat: [#3] WebSecurityConfig, filter 틀 추가

---
+++ src/main/java/com/sparta/barointern9/filter/CustomAuthorizationFilter.java
@@ -0,0 +1,23 @@
+package com.sparta.barointern9.filter;
+
+import com.sparta.barointern9.jwt.JwtUtil;
+import com.sparta.barointern9.service.UserDetailsServiceImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class CustomAuthorizationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    }
+}
