d0ef45ba494e5fe1a26942b649e422ee8fe95021
inzulmi <inzulmi132@gmail.com>
2025-02-14 23:05:44 +0900

    refactor: [#22] Authority 리펙터링

--- src/main/java/com/sparta/barointern9/service/AuthorityService.java
+++ src/main/java/com/sparta/barointern9/service/AuthorityService.java
@@ -3,10 +3,7 @@ package com.sparta.barointern9.service;
 import com.sparta.barointern9.entity.Authority;
 import com.sparta.barointern9.entity.User;
 import com.sparta.barointern9.enums.UserRole;
-import com.sparta.barointern9.exception.CustomApiException;
-import com.sparta.barointern9.exception.ErrorCode;
 import com.sparta.barointern9.repository.AuthorityRepository;
-import com.sparta.barointern9.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
@@ -18,15 +15,11 @@ import java.util.Collection;
 @RequiredArgsConstructor
 public class AuthorityService {
 
-    private final UserRepository userRepository;
     private final AuthorityRepository authorityRepository;
 
     public Collection<GrantedAuthority> getAuthorities(String username) {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new CustomApiException(ErrorCode.USER_NOT_FOUND));
-
         Collection<GrantedAuthority> authorities = new ArrayList<>();
-        authorityRepository.findAllByUser(user)
+        authorityRepository.findAllByUserUsername(username)
                 .forEach(authority -> authorities.add(authority::toString));
         return authorities;
     }
