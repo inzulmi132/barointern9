7ca60e620f6339cf014e693eb7138abdd8124d7e
inzulmi <inzulmi132@gmail.com>
2025-02-16 18:00:27 +0900

    test: [#26] User Controller Test 구현

--- src/main/java/com/sparta/barointern9/dto/request/SignRequestDto.java
+++ src/main/java/com/sparta/barointern9/dto/request/SignRequestDto.java
@@ -1,8 +1,12 @@
 package com.sparta.barointern9.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignRequestDto {
     private String username;
     private String password;
